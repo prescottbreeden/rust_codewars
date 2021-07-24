ade81e357cbbb9d586992cd72cfd6ef1fa588182
Peter Lansdaal <peter@amplifycp.com>
2021-07-23 17:42:59 -0700

    changes from peter

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 
 fn main() {
-    let input = vec![ (0,1) , (7,8) , (1,2) , (6,7) , (0,3) , (5,8) , (3,4) , (1,4) , (4,5) , (2,5) , (4,7) , (3,6) ];
+    let input = vec![(0,1),(5,9),(9,8),(13,9),(7,11),(0,4),(9,10),(10,6),(4,5),(2,3),(13,14),(4,8),(8,12),(1,5),(5,6),(14,15),(10,11),(2,6),(12,13),(1,2),(10,14),(15,11),(6,7),(3,7)];
+    //let input = vec![(0,1),(7,8),(1,2),(6,7),(0,3),(5,8),(3,4),(1,4),(4,5),(2,5),(4,7),(3,6)];
+    //let input = vec![(0,1),(1,2),(2,5),(5,4),(4,7),(7,8),(8,5),(1,4),(6,7),(3,6),(3,0),(3,4)];
+    //let input = vec![(0,1),(7,8),(1,2),(6,7),(0,3),(8,5),(3,4),(4,1),(4,5),(2,5),(3,6),(7,4)];
+    //let input = vec![(5,9),(6,10),(9,13),(7,11),(4,5),(5,6),(6,7),(8,12),(1,5),(2,6),(8,9),(10,14),(10,11),(4,8),(12,13),(9,10),(1,2),(11,15),(13,14),(14,15),(0,4),(0,1),(3,7),(2,3)];
     let result = dots_and_boxes(input);
     println!("{:?}", result);
 }
 
 pub fn dots_and_boxes(r: Vec<(usize,usize)>) -> (usize,usize) {
-    let mut player1 = (1, 0);
-    let mut player2 = (2, 0);
-    let mut current_player = player1;
+    let mut scores = [0, 0];
 
     let boxy = |n: usize, i: usize| -> Vec<(usize, usize)> { 
-        let x = i + (i + 1)/n % n;
+        let x = i + i/(n - 1);
         vec![
             (x, x + 1),         // (0, 1)
             (x + 1, x + 1 + n), // (1, 4)
@@ -20,103 +22,46 @@ pub fn dots_and_boxes(r: Vec<(usize,usize)>) -> (usize,usize) {
         ]
     };
 
-    let n = ((r.len() + 1) as f64).sqrt() as usize;
-    let boxes = usize::pow(n - 1, 2);
+    let nodes = r.clone().into_iter().fold(0, |acc, f| {
+        if f.0 > acc && f.0 > f.1 {
+            return f.0
+        } else if f.1 > acc && f.1 > f.0 {
+            return f.1
+        }
+        acc
+    }) + 1;
+    let length_of_grid = (nodes as f64).sqrt() as usize;
+
+    let boxes = usize::pow(length_of_grid - 1, 2);
 
     let mut results: Vec<Vec<(usize, usize)>> = vec![0; boxes].into_iter().enumerate().map(|(index, _)| {
-        boxy(n, index)
+        boxy(length_of_grid, index)
     }).collect();
 
+
+    let mut turn = 0;
     for (x, y) in r {
         for i in 0..results.len() {
             results[i] = results[i].clone().into_iter().filter(|(a, b)| {
                 !((a == &x && b == &y) || (a == &y && b == &x))
             }).collect();
         }
-        println!("{:?}", results);
-        let change_player = results.into_iter().fold(true, |acc, curr| {
-            if curr.len() == 0 {
-                current_player.1 += 1;
-                return false
+
+        let stay = results.clone().into_iter().fold(false, |acc, b| {
+            if b.len() == 0 {
+                scores[turn] += 1;
+                return true;
             }
             acc
         });
-        if change_player {
-            if current_player.0 == 1 {
-                current_player = player2;
-            } else {
-                current_player = player1;
-            }
+        if !stay {
+            turn = (turn + 1) % 2;
         }
+
+        results = results.into_iter().filter(|b| {
+            b.len() > 0
+        }).collect();
     }
-
-    (0, 1)
-
-    /*
-        currentPlayer = player1
-
-        // player tuple = (pos, score)
-        player1 = (1, 0)
-        player2 = (2, 0)
-
-        boxes = [
-                [(4, 3), (0, 3)]
-                [(1, 2)]
-            ]
-
-            1: [(0, 1), (1, 4), (4, 3), (0, 3)]
-            1: [(Box - 1, Box), (Box, Box + len), (Box + len, len), (Box - 1, len)]
-
-            2: [(1, 2), (2, 5), (5, 4), (4, 1)]
-            2: [(Box - 1, Box), (Box - 1 , Box + len), (Box + len, Box - 1 + len), (Box - 1 + len, Box - 1)]
-
-            3: [(3, 4), (4, 7), (7, 6), (6, 3)]
-            3: i => mult 1 -> add 1 -> add len -> mult 1 -> sub 1 -> mult 1 -> sub len
-
-            3: i => (mult 1 -> add 1) -> (mult 1 -> add len) -> (mult 1 -> sub 1) -> (mult 1 -> sub len)
-
-
-            let 3a: i => (i + floor(((i + 1)/len) % len)) -> (                                , add 1)
-            let 3b: i => (i + floor(((i + 1)/len) % len)) -> (-> add 1 ->                     , add len)
-            let 3c: i => (i + floor(((i + 1)/len) % len)) -> (-> add 1 -> -> add len          , sub 1)
-            let 3d: i => (i + floor(((i + 1)/len) % len)) -> (-> add 1 -> -> add len -> sub 1 , sub len)
-
-
-
-
-// --[ game logic ]--
-
-    filter all boxes for (0, 1) && (1, 0)
-
-    let changePlayer = fold(true, (acc, curr) -> 
-        if curr.len == 0 
-            then currentPlayer.1 += 1
-            acc = false
-        acc
-    )
-
-    filter len > 0
-
-    if changePlayer then currentPlayer = the other player
-
-
-// --[ sample input ]--
-
-        [ (0,1)
-        , (7,8)
-        , (1,2)
-        , (6,7)
-        , (0,3)
-        , (5,8)
-        , (3,4)
-        , (1,4)
-        , (4,5)
-        , (2,5)
-        , (4,7)
-        , (3,6)
-        ] 
-
-    */
-
+    (scores[0], scores[1])
 }
 
